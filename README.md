070918e0d3002dbf682cdffd7cc8862a2f46eb1b
Jake Halaska <26490690+5Dev24@users.noreply.github.com>
2021-09-08 17:16:38 -0500

    Added work

--- README.md
+++ README.md
@@ -1,2 +1,172 @@
 # EqualBot
-A Discord bot that brings true equality
+
+### A bot to bring true equality
+
+<br>
+
+## How to install
+
+Have `git`, `python3`, and `pip3` install from your local package manager.
+
+```bash
+# Clone repo
+git clone https://github.com/5Dev24/EqualBot
+
+# Move into repo
+cd ./EqualBot/
+
+# Change virtual environment
+python3 -m venv ./venv/
+
+# Active virtual environment
+source ./venv/bin/active
+
+# Install python dependencies
+pip3 install -r ./requirements.txt
+
+# While the bot's code is installed, it will not yet run. Follow the setup steps below to get it working
+```
+
+<br>
+
+## How to setup
+
+### Setting the bot's token
+
+```bash
+# This step can be skipped so long as the environement variable ClientSecret is set to your bot's secret token
+
+# Run initially to generate setup files
+# Should terminate with a message telling you to set your bot's secret token
+python3 ./src/__main__.py
+
+# Get your bot's secret token
+
+# Replace "[PLACE YOUR TOKEN HERE]" with your bot's secret token
+echo "ClientSecret=[PLACE YOUR TOKEN HERE]" > ./src/.env
+```
+
+<br>
+
+### Configuring the bot to your server
+
+You'll need to have enable `Developer Mode` on Discord in order to view the ids of everything. 
+To enable it, go to  `User Settings` > `Advanced` > `Developer Mode` and turn it on.
+
+```bash
+# Use your favorite GUI or command line text editor for these next few steps
+# I'll be using nano as an example
+
+nano ./config/bot.json
+```
+
+<br>
+
+Right click on the text channel you want to have as the leaderboard and copy it's id
+
+Find the line that contains: "Leaderboard Channel ID": null
+
+Replace the null with the channel id for the leaderboard channel (leave the comma that comes after the null)
+
+<br>
+
+Right click on the text channel you want to have as the equal channel and copy it's id
+
+Find the line that contains: "Equal Channel ID": null
+
+Replace the null with the channel id for the equal channel (leave the comma that comes after the null)
+
+<br>
+
+Next you'll need your equal role, this can be used to prevent people from reacting, sending tts, adding attachments, etc. You can find my roles at the end of this guide. Note that the server only has this bot running on it.
+
+Go to Server Settings > Roles, right click on the role you want for the equal role and copy it's id
+
+Note: this role will be given to all people that join the server but not those who are already in the server
+
+Find the line that contains: "Equal Role ID": null
+
+Replace the null with the role id for the equal role (leave the comma that comes after the null)
+
+```bash
+# Now save and close nano by pressing Ctrl + X, then Ctrl + Y, and pressing Enter
+```
+
+### Running the bot
+
+I don't recommend a docker or any containerization because a virtual python environment should isolate it package wise from the system.
+
+```bash
+# Active virtual environment
+source ./venv/bin/active
+
+# Run bot
+python3 ./src/__main__.py
+
+# or, because the Python3 env shebang at the top of the file
+./src/__main__.py # If you can't run it, use `chmod +x ./src/__main__.py` to make the file executable
+```
+
+I used the following script to start mine. The main thing is to remember to have the bash shebang else you'll run into issues with it saying that `source` cannot being found.
+
+```bash
+#!/bin/bash
+source ./venv/bin/active
+python3 ./src/__main__.py
+```
+
+Also running this as a sevice works so you don't have to manually start it everytime your server comes back online. Example below is mine.
+
+```
+[Unit]
+Description=EqualBot
+After=network.target
+StartLimitBurst=3
+StartLimitIntervalSec=20
+
+[Service]
+WorkingDirectory=/opt/EqualBot/
+ExecStart=/bin/bash /opt/EqualBot/start.sh
+Restart=on-failure
+RestartSec=3s
+
+[Install]
+WantedBy=multi-user.target
+```
+
+### Congratulations
+
+You've install, setup, configured, and ran the bot!
+
+Welcome to true equality
+
+## My Discord Setup
+
+(If I list it then it's turned on, if not, I have it off)
+
+Roles:
+* Equal (Bot role)
+	- `Administrator`
+
+* Equal (User role)
+	- `Display role members separately...`
+	- `View Channels`
+	- `Create Invite`
+	- `Send Messages`
+	- `Read Message History`
+
+* Muted
+	- `View Channels`
+	- `Create Invite`
+	- `Read Message History`
+
+* @everyone
+	(nothing, no permissions set)
+
+Channels:
+* Equal (category)
+	- #equal (text channel)
+
+* Chaos (category)
+	- #leaderboard (text channel)
+		* No role can `Send Messages`
---
+++ src/__main__.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from typing import Union
+import datetime
+import discord
+import dotenv
+import time
+import json
+import os
+
+def _read_json(path: str) -> dict:
+	with open(path, "r") as json_file:
+		return json.load(json_file)
+
+def _write_json(path: str, data: dict, neat: bool = False):
+	with open(path, "w") as json_file:
+		if neat:
+			json.dump(data, json_file, indent = 4)
+		else:
+			json.dump(data, json_file)
+
+_project_root = Path(os.path.abspath(__file__)).parent.parent.__str__()
+_months = (
+	"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec",
+	"january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december",
+	"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
+)
+
+class EqualBot(discord.Client):
+
+	def __init__(self):
+		super().__init__(intents = discord.Intents(messages = True, members = True, guilds = True))
+
+		self.chaos_file_path = os.path.join(_project_root, "data", "chaos.json")
+
+		bot_config = _read_json(os.path.join(_project_root, "config", "bot.json"))
+
+		self.historical_search_threshold = bot_config.get("Historical Search Threshold (in seconds)", 30)
+		self.leaderboard_channel_id = bot_config.get("Leaderboard Channel ID", None)
+		self.equal_channel_id = bot_config.get("Equal Channel ID", None)
+		self.equal_role_id = bot_config.get("Equal Role ID", None)
+		self.historical_purge = bot_config.get("Historical Purge", False)
+
+		birthday_data = _read_json(os.path.join(_project_root, "data", "birthdays.json"))
+
+		self.birthday_cache = [ # 2d matrix making a calendar
+			[0] * 31, [0] * 29, [0] * 31, [0] * 30,
+			[0] * 31, [0] * 30, [0] * 31, [0] * 31,
+			[0] * 30, [0] * 31, [0] * 30, [0] * 31
+		]
+
+		for birthday in birthday_data.values():
+			if not isinstance(birthday, dict):
+				raise ValueError("Invalid birthday entry, must be a dictionary")
+
+			try:
+				month = int(birthday.get("month", 13))
+			except ValueError:
+				raise ValueError(f"Invalid month, must be a number, not \"{birthday.get('month')}\"")
+
+			try:
+				day = int(birthday.get("day", 32))
+			except ValueError:
+				raise ValueError(f"Invalid day, must be a number, not \"{birthday.get('day')}\"")
+
+			if month < 1 or month > 12:
+				raise ValueError(f"Invalid month \"{month}\", must be 1 <= month <= 12")
+
+			if day < 1 or day > self.birthday_cache[month - 1].__len__():
+				raise ValueError(f"Invalid day \"{day}\", must be 1 <= day <= {self.birthday_cache[month - 1].__len__()}")
+
+			self.birthday_cache[month - 1][day - 1] += 1
+
+	async def increase_chaos(self, user_id: Union[int, str], user_name: str):
+		user_id = str(user_id)
+
+		chaos_data = _read_json(self.chaos_file_path)
+
+		if user_id not in chaos_data:
+			chaos_data[user_id] = {"name": user_name, "points": 0}
+
+		chaos_data[user_id]["points"] += 1
+
+		_write_json(self.chaos_file_path, chaos_data)
+
+		await self.update_leaderboard()
+
+	async def decrease_chaos(self, user_id: Union[int, str], user_name: str):
+		user_id = str(user_id)
+
+		chaos_data = _read_json(self.chaos_file_path)
+
+		if user_id not in chaos_data:
+			chaos_data[user_id] = {"name": user_name, "points": 0}
+
+		chaos_data[user_id]["points"] -= 1
+
+		if chaos_data[user_id]["points"] < 0: # Prevent negative
+			chaos_data[user_id]["points"] = 0
+
+		_write_json(self.chaos_file_path, chaos_data)
+
+		await self.update_leaderboard()
+
+	async def generate_leaderboard_messaage(self) -> str:
+		chaos_data = _read_json(self.chaos_file_path)
+
+		longest_username_length = max(chaos_data.values(), key = lambda x: x["name"].__len__())["name"].__len__()
+		longest_chaos_points = max(chaos_data.values(), key = lambda x: x["points"].__str__().__len__())["points"].__str__().__len__()
+
+		output = "```md"
+
+		for user_data in sorted(chaos_data.values(), key = lambda x: x["points"], reverse = True):
+			output += f"\n[ {user_data['name']:<{longest_username_length}} ][ {user_data['points']:>{longest_chaos_points}} ]"
+
+		return output + "\n```"
+
+	async def update_leaderboard(self):
+		leaderboard: discord.TextChannel = self.get_channel(self.leaderboard_channel_id)
+
+		if leaderboard is None:
+			print(f"Unable to update leaderboard, couldn't find channel with id \"{self.leaderboard_channel_id}\"")
+			return
+
+		async for message in leaderboard.history(oldest_first = True, limit = None):
+			if message.author.id == self.user.id:
+				await message.edit(content = await self.generate_leaderboard_messaage())
+				return
+
+		await leaderboard.send(content = await self.generate_leaderboard_messaage())
+
+	def update_last_online(self):
+		_write_json(os.path.join(_project_root, "data", "last_online.json"), int(time.time()))
+
+	async def on_ready(self):
+		print(f"Readying as {self.user} ({self.user.id})")
+
+		print(f"Use this link to join your bot to your server: https://discord.com/api/oauth2/authorize?client_id={self.user.id}&permissions=8&scope=bot")
+
+		await self.change_presence(activity = discord.Game("God"))
+
+		last_online_path = os.path.join(_project_root, "data", "last_online.json")
+
+		should_run_historical_search = False
+
+		if os.path.isfile(last_online_path):
+			last_online = _read_json(last_online_path)
+
+			delta = int((datetime.datetime.now() - datetime.datetime.fromtimestamp(last_online)).total_seconds())
+
+			if delta >= self.historical_search_threshold:
+				print("\tLast online ", delta, " second", "" if delta == 1 else "s", " ago which is greater than or equal to ", self.historical_search_threshold, " second", "" if self.historical_search_threshold == 1 else "s", sep="")
+				should_run_historical_search = True
+		else:
+			should_run_historical_search = True
+
+		self.update_last_online()
+
+		if should_run_historical_search or not os.path.isfile(self.chaos_file_path): # Trigger chaos points historical search
+			print("\tRunning historical search to calculate chaos points")
+			chaos_data = {}
+
+			equal_channel: discord.TextChannel = self.get_channel(self.equal_channel_id)
+
+			if equal_channel is not None:
+				message: discord.Message
+				async for message in equal_channel.history(limit = None):
+					author_id = str(message.author.id)
+					if author_id not in chaos_data:
+						chaos_data[author_id] = {"name": message.author.name, "points": 0}
+
+					check = await self.check_message(message)
+					if check == 1:
+						chaos_data[author_id]["points"] += 1
+
+					elif check == -1:
+						if self.historical_purge:
+							print("\t\tPurging message by ", message.author.name, " (", message.author.id, ") saying ", message.clean_content, sep = "")
+							await message.delete()
+						else:
+							print("\t\tNot purging bad message at", message.jump_url)
+
+						chaos_data[author_id]["points"] -= 1
+
+				print("\tSaving new chaos data")
+				_write_json(self.chaos_file_path, chaos_data)
+
+		await self.update_leaderboard()
+
+		print("Readied")
+
+	async def check_message(self, message: discord.Message) -> int:
+		if message.author.id == self.user.id: # Don't listen to myself by mistake
+			return 0
+
+		if message.channel is not None:
+			if message.channel.id != self.equal_channel_id: # Equal text channel
+				return 0
+
+			if message.reference is not None and not message.is_system(): # Prevent replies
+				return -1
+
+			if message.attachments.__len__(): # Don't allow attachments
+				return -1
+
+			if message.reactions.__len__(): # Don't allow reactions
+				await message.clear_reactions() # Auto fix
+
+			content = message.clean_content
+
+			if content == "Equal":
+				return 1
+
+			day = message.created_at
+
+			if content == "Equal 🎄" and day.month == 12 and day.day == 25: # Allow Christmas emote
+				return 1
+
+			if content == "Equal 🎂": # Allow birthdays
+				if self.birthday_cache[day.month - 1][day.day - 1]: # 2d matrix of birthdays, zero-based
+					return 1
+
+			return -1
+
+	async def message_handle(self, message: discord.Message, edited: bool = False):
+		if message.guild is None: # Unmanaged, DMs
+			contents = message.clean_content.lower().replace("\n", " ").split(" ")
+
+			cmd, args = contents[0], contents[1:]
+			del contents
+
+			if cmd == "bday": # Should I check if someone changes their bday a bunch to be able to post 🎂's? No, rate limiting for something like this? No
+				if args.__len__() == 2:
+					month, day = args
+					del args
+
+					try:
+						month = _months.index(month) % 13
+					except ValueError:
+						await message.reply(embed = discord.Embed(title = "bday Command", description = "Invalid month", color = discord.Color.red())\
+							.add_field(name = "month", value = f"The month can be a three letter abbreviation (Jan, Feb, Nov, or Dec), full names (April, August, September, or July), or a number (3, 5, 6, or 10)\n\nNot \"{month}\""), mention_author=False)
+					else:
+						try:
+							day = int(day) # Could check length before too as a number over 2 digits isn't allowed
+						except ValueError:
+							await message.reply(embed = discord.Embed(title = "bday Command", description = "Invalid day", color = discord.Color.red())\
+								.add_field(name = "day", value = f"The day must be a number\n\nNot \"{day}\""), mention_author=False)
+
+						
+						if day < 1 or day > self.birthday_cache[month].__len__():
+							await message.reply(embed = discord.Embed(title = "bday Command", description = "Invalid day", color = discord.Color.red())\
+								.add_field(name = "day", value = f"The day must be between 1 and {self.birthday_cache[month].__len__()}, {day} isn't"), mention_author=False)
+						else:
+							user_id = str(message.author.id)
+							birthday_data = _read_json(os.path.join(_project_root, "data", "birthdays.json"))
+
+							if user_id in birthday_data:
+								old_birthday = birthday_data[user_id].copy()
+
+								if old_birthday.get("month") == month + 1 and old_birthday.get("day") == day: # Same day
+									await message.reply(embed = discord.Embed(title = "bday Command", description = f"{_months[month + 12].title()} {day} is already your birthday!", color = discord.Color.red()), mention_author=False)
+									self.update_last_online()
+									return
+
+							self.birthday_cache[month][day - 1] += 1 # Modify loaded cache
+							birthday_data[user_id] = {"month": month + 1, "day": day}
+							_write_json(os.path.join(_project_root, "data", "birthdays.json"), birthday_data)
+
+							if user_id in birthday_data:
+								self.birthday_cache[old_birthday.get("month") - 1][old_birthday.get("day") - 1] -= 1 # Modify loaded cache
+
+								await message.reply(embed = discord.Embed(title = "bday Command", description = "Birthday set!", color = discord.Color.green())\
+									.add_field(name = "Updated from", value = f"{_months[old_birthday.get('month') + 11].title()} {old_birthday.get('day')}")\
+									.add_field(name = "To", value = f"{_months[month + 12].title()} {day}"), mention_author=False)
+
+							else:
+								await message.reply(embed = discord.Embed(title = "bday Command", description = "Birthday set!", color = discord.Color.green())\
+									.add_field(name = "Birthday set to", value = f"{_months[month + 12].title()} {day}"), mention_author=False)
+				else:
+					await message.reply(embed = discord.Embed(title = "bday Command", description = "Use `bday` **<**`month`**>** **<**`day`**>**", color = discord.Color.dark_blue())\
+						.add_field(name = "month", value = "The month can be a three letter abbreviation (Jan, Feb, Nov, or Dec), full names (April, August, September, or July), or a number (3, 5, 6, or 10)", inline = False)\
+						.add_field(name = "day", value = "The day must be a number (1 to 28/29/30/31)", inline = False), mention_author=False)
+
+		else: # Managed, Guilds
+			check = await self.check_message(message)
+
+			if check == 1:
+				if not edited: # Prevent infinite points from editing messages on Christmas or a birthday
+					await self.increase_chaos(message.author.id, message.author.name)
+
+			elif check == -1:
+				await message.delete()
+
+				if edited: # They would've gained chaos so take away chaos
+					await self.decrease_chaos(message.author.id, message.author.name)
+		
+		self.update_last_online()
+
+	async def on_message(self, message: discord.Message):
+		await self.message_handle(message)
+
+	async def on_message_edit(self, _, after_message: discord.Message):
+		await self.message_handle(after_message, True)
+
+	async def on_member_join(self, member: discord.member.Member):
+		equal_role = discord.utils.get(member.guild.roles, id = self.equal_role_id) # Get Equal role
+
+		if equal_role is None:
+			print(f"Unable to assign Equal role as a role couldn't be found with id \"{self.equal_role_id}\"")
+		else:
+			await member.edit(nick = "Equal", roles = [equal_role], reason = "To make us all Equal") # Set nickname and roles
+
+def main():
+	config_path = os.path.join(_project_root, "config")
+	if not os.path.isdir(config_path):
+		os.makedirs(config_path, 0o744, True)
+
+	bot_config_path = os.path.join(config_path, "bot.json")
+	if not os.path.isfile(bot_config_path):
+		_write_json(bot_config_path, {
+			"Historical Search Threshold (in seconds)": 600,
+			"Leaderboard Channel ID": None,
+			"Equal Channel ID": None,
+			"Equal Role ID": None,
+			"Historical Purge": False
+		}, True) # Write default config so we don't have to ship it
+
+	data_path = os.path.join(_project_root, "data")
+	if not os.path.isdir(data_path):
+		os.makedirs(data_path, 0o744, True)
+
+	dotenv.load_dotenv()
+
+	if os.getenv("ClientSecret") is None:
+		env_file_path = os.path.join(_project_root, "src", ".env")
+		if os.path.isfile(env_file_path):
+			with open(env_file_path, "a") as env_file:
+				env_file.write("\nClientSecret=<bot secret token>")
+		else:
+			with open(env_file_path, "w") as env_file:
+				env_file.write("ClientSecret=<bot secret token>")
+
+		raise ValueError(f"Set the ClientSecret value inside of the .env file at {os.path.join(_project_root, 'src')} to your bot's secret token")
+
+	if os.getenv("ClientSecret") == "<bot secret token>":
+		raise ValueError(f"Set the ClientSecret value inside of the .env file at {os.path.join(_project_root, 'src')} to your bot's secret token")
+
+	EqualBot().run(os.getenv("ClientSecret"))
+
+if __name__ == "__main__": main()
